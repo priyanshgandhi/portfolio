cf43d8285f9b57e907570ac9f8c0f6a09477cd85
Priyansh Gandhi <priyanshgandhi.pg@gmail.com>
2023-08-30 14:46:34 +0530

    database update

---
+++ submit.php
@@ -0,0 +1,33 @@
+<?php
+// Database connection information
+$servername = "localhost";
+$username = "portfolioadmin";
+$password = "admin";
+$dbname = "portfolio";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get data from the form
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+// Prepare and execute SQL statement to insert data into the database
+$sql = "INSERT INTO contact_messages (name, email, subject, message) VALUES ('$name', '$email', '$subject', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the database connection
+$conn->close();
+?>
